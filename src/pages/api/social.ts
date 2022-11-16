300b71880b9b10d4df29ab5d27226cd00ec7e4a7
gsborgz <borgesgabrielsilva@gmail.com>
2022-11-16 11:27:25 -0300

    modificando usernames

--- src/pages/api/social.ts
+++ src/pages/api/social.ts
@@ -14,13 +14,13 @@ export default function handler(
 ) {
   if (req.method === 'GET') {
     res.status(200).json([
+      { name: 'Twitch', description: '', icon: 'twitch.svg', url: 'https://www.twitch.tv/oborgez' },
+      { name: 'TikTok', description: '', icon: 'tiktok.svg', url: 'https://www.tiktok.com/@gsborgz' },
+      { name: 'YouTube', description: '', icon: 'youtube.svg', url: 'https://www.youtube.com/@gsborgz' },
       { name: 'Twitter', description: '', icon: 'twitter.svg', url: 'https://twitter.com/gsborgz' },
       { name: 'Instagram', description: '', icon: 'instagram.svg', url: 'https://www.instagram.com/gsborgz/' },
       { name: 'Facebook', description: '', icon: 'facebook.svg', url: 'https://www.facebook.com/gsborgz/' },
-      { name: 'TikTok', description: '', icon: 'tiktok.svg', url: 'https://www.tiktok.com/@gsborgz' },
-      { name: 'Twitch', description: '', icon: 'twitch.svg', url: 'https://www.twitch.tv/oborgez' },
-      { name: 'YouTube', description: '', icon: 'youtube.svg', url: 'https://www.youtube.com/@oborgez' },
-      { name: 'GitHub', description: '', icon: 'github.svg', url: 'https://github.com/gabrielSBorges' },
+      { name: 'GitHub', description: '', icon: 'github.svg', url: 'https://github.com/gsborgz' },
     ])
   }
 }
